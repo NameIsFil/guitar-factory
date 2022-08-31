23d43de85454ab16e35099f2f8abaa08532b0461
NameIsFill <filpopowski@gmail.com>
2022-08-31 02:26:35 +0200

    NeckSupplier class rework

--- src/Supplier.js
+++ src/Supplier.js
@@ -1,19 +1,51 @@
-class Supplier {
+class NeckSupplier {
+  lastSupplyTimestamp = null;
+  frequencyInSeconds;
+  supplyCount = null;
+
+
+  constructor(frequencyInSeconds) {
+    this.frequencyInSeconds = frequencyInSeconds;
+    this.neckDelivery();
+  }
+
+  neckDelivery() {
+    const neckTimeDifference = Date.now() - this.lastSupplyTimestamp;
+    if (neckTimeDifference < this.frequencyInSeconds * 1000) {
+      this.supplyCount = null;
+      return this.supplyCount;
+    } else {
+      const min = 3;
+      const max = 6;
+      const arrayOfSupplies = [];
+      this.lastSupplyTimestamp = Date.now();
+      const randomNumber = Math.round(Math.random() * (max - min) + min);
+      for (let i = 0; i < randomNumber; i++) {
+        arrayOfSupplies.push('part');
+      }
+      this.supplyCount = arrayOfSupplies.length;
+      return this.supplyCount;
+    }
+  }
+}
+
+class StringsSupplier {
   lastSupplyTimestamp = null;
   frequencyInSeconds;
   supplyCount = null;
 
   constructor(frequencyInSeconds) {
     this.frequencyInSeconds = frequencyInSeconds;
-    this.delivery();
+    this.stringsDelivery();
   }
 
-  delivery() {
-    const timeDifference = Date.now() - this.lastSupplyTimestamp;
-    if (timeDifference < this.frequencyInSeconds * 1000) {
+  stringsDelivery() {
+    const stringsTimeDifference = Date.now() - this.lastSupplyTimestamp;
+    if (stringsTimeDifference < this.frequencyInSeconds * 1000) {
       this.supplyCount = null;
       return this.supplyCount;
     } else {
+      const array = [];
       const min = 3;
       const max = 6;
       this.lastSupplyTimestamp = Date.now();
@@ -23,16 +55,4 @@ class Supplier {
   }
 }
 
-class NeckSupplier extends Supplier {
-  constructor(frequencyInSeconds) {
-    super(frequencyInSeconds);
-  }
-}
-
-class StringsSupplier extends Supplier {
-  constructor(frequencyInSeconds) {
-    super(frequencyInSeconds);
-  }
-}
-
-export { Supplier, StringsSupplier, NeckSupplier };
+export { StringsSupplier, NeckSupplier };
